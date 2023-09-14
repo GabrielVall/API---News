4de5ef350c1c5d1935c2de8860c19567b8562523
= <gabrielvallejo2000@gmail.com>
2023-09-14 11:03:45 -0500

    helmet csurf

--- src/app.js
+++ src/app.js
@@ -1,10 +1,40 @@
 const express = require('express');
-const usuariosRoutes = require('./routes/usuariosRoutes');
+const mysql = require('mysql2');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Middlewares de seguridad
+app.use(helmet()); // Esto añade varias capas de seguridad configurando encabezados HTTP.
+app.use(csrf()); // Protección contra CSRF
+
 // Middleware para parsear el cuerpo del request en formato JSON
 app.use(express.json());
+
+// Configuración de la conexión
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Intento de conexión
+connection.connect(error => {
+  if (error) {
+    console.error('Error conectando a la base de datos:', error.stack);
+    return;
+  }
+  console.log('Conectado a la base de datos con el ID', connection.threadId);
+});
+
+// Añadir variable CSRF a todas las respuestas
+app.use((req, res, next) => {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 // Ruta por defecto
 app.get('/', (req, res) => {
@@ -17,10 +47,16 @@ app.get('/', (req, res) => {
       <li>PUT /usuarios/:id</li>
       <li>DELETE /usuarios/:id</li>
     </ul>
+    <form action="/usuarios" method="post">
+      <input type="hidden" name="_csrf" value="${res.locals.csrfToken}">
+      <!-- otros campos del formulario -->
+      <button type="submit">Crear Usuario</button>
+    </form>
   `);
 });
 
-// Rutas
+// Rutas (por ahora solo tienes la de usuarios, pero podrías añadir más en el futuro)
+const usuariosRoutes = require('./routes/usuariosRoutes');
 app.use('/usuarios', usuariosRoutes);
 
 app.listen(PORT, () => {
